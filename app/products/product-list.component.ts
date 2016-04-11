af934b8f461337fb9b311ff17fbe217dd74fac8e
shmiko <shmiko@gmail.com>
2016-04-11 11:29:28 +1000

    yello discography

---
+++ app/products/product-list.component.ts
@@ -0,0 +1,39 @@
+import { Component } from 'angular2/core';
+
+@Component({
+	selector: 'pm-products',
+	templateUrl: 'app/products/product-list.component.html'
+})
+
+export class ProductListComponent {
+	pageTitle: string = 'Item List';
+	imageWidth: number = 50;
+	imageHeight: number = 25;
+	imageMargin: number = 2;
+	showImage: boolean = true;
+	listFilter: string = 'cart';
+	items: any[] = [{
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2016",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+		"productId": 2,
+		"productName": "Garden Cart",
+		"productCode": "GDN-0023",
+		"releaseDate": "March 18, 2016",
+		"description": "15 gallon capacity rolling garden cart",
+		"price": 32.99,
+		"starRating": 4.2,
+		"imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+	}];
+	//add methods after properties
+	toggleImage(): void{
+		this.showImage = !this.showImage;
+	}
+}
